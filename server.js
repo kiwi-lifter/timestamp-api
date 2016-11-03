2484e14548bb8cdae87cd8113c3440406faf65f8
Garrick McCaskill <kiwilifter808@gmail.com>
2016-11-03 01:54:27 +0000

    Add strftime library for date formatting

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
-
+var strftime = require('strftime');
 var app = express();
+console.log(strftime('%B %d, %Y %H:%M:%S')); // => April 28, 2011 18:21:08
+console.log(strftime('%F %T', new Date(1307472705067))); // => 2011-06-07 18:51:45
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   
   
   
   
-  res.send('Hello World!');
+  res.send('Output goes here!');
 });
 
 app.listen(port, function () {
