35d4a29b9d13abb96d96f4412f3e9ef54b0f8b52
Garrick McCaskill <kiwilifter808@gmail.com>
2016-11-04 10:23:04 +0000

    Add get request with anonymous function that returns formatted timestamp from url param

--- server.js
+++ server.js
@@ -5,19 +5,42 @@
  * @returns the Unix timestamp and the natural language form of the date or null
  **/
  
-var express = require('express');
-var strftime = require('strftime');
-var app = express();
+var express = require('express')
+  ,strftime = require('strftime')
+  ,app = express();
 
-
-app.get('/', function (req, res) {
+app.get("/", (req,res) => {
   
+  res.send( "<h1>API Basejump: Timestamp microservice</h1><p>For more information visit this <a href='https://timestamp-ms.herokuapp.com/'>link</a></P>");
   
-  
-  
-  res.send('Output goes here!');
 });
 
+app.get('/:query', (req, res) => {
+  
+  var query = req.params.query
+  var output = {}
+  var time;
+  if (isNaN(Number(query))) { //if not timestamp
+    if (new Date(query) !== 'Invalid Date') { //if valid date
+      time = new Date(query)
+    } else {
+      time = null
+    }
+  } else { //if timestamp
+    console.log(Number(query))
+    time = new Date(Number(query) * 1000)
+  }
+
+  if (time === null) {
+    output = { 'unix': null, 'natural': null }
+  } else {
+    output = { 'unix': time.getTime() / 1000, 'natural': strftime('%B %d, %Y', time) }
+  }
+  console.log(output)
+  res.send(JSON.stringify(output))
+    
+  });
+ 
 app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!');
 });
